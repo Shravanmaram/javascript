7e48961f5170fd7989fc16e20bfd097f4727396d
Maram Shravan Kumar <shravanmaram4@gmail.com>
2024-06-28 21:14:10 +0530

    Add files via upload

---
+++ forloops.js
@@ -0,0 +1,33 @@
+let a=1;
+// for loop its for array
+for (let i=0;i<100;i++){
+    console.log(a+i)
+}
+// creating object
+ let obj={
+    name:"shravan",
+    role:"developer",
+ }
+//for in loop its for objects
+for (const key in obj){
+    console.log(key)
+}
+// for of it used for charecter
+for (const c of "shravan") {
+    console.log(c)
+    
+}
+// while loop
+let i=0;
+while (i<6) {
+    console.log(i)
+    i++;
+    
+}
+do {
+    console.log(i)
+        i++;
+    
+} while(i<6);
+
+    
