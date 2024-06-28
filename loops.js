2a54279cd8512720e3f1002649cb0685065a6a5b
Maram Shravan Kumar <shravanmaram4@gmail.com>
2024-06-28 19:29:06 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,49 @@
+console.log("iam loop tutorial");
+
+let age = 10;
+let grace = 12;
+
+// if loop
+if (age > 18) {
+  console.log("you can drive");
+} else {
+  console.log("you cant drive");
+}
+
+// operations
+console.log(age + grace);
+console.log(age - grace);
+console.log(age / grace);
+console.log(age * grace);
+console.log(age ** grace);
+console.log(age % grace);
+
+if (age != 18) {
+  console.log("you can drive");
+} else if (age == 0) {
+  console.log("are you kidding me??");
+} else {
+  console.log("you cant drive");
+}
+
+
+if (age = 18) {
+    console.log("you can drive");
+  } else if (age == 0) {
+    console.log("are you kidding me??");
+     }   else if (age == 0) {
+        console.log("are you again kidding me??");
+  } else {
+    console.log("you cant drive");
+  }
+ a=20;
+ b=15;
+let c= a > b ? (a - b) : (b - a);
+// // translates to
+// if (a>b){
+//     console.log(a-b)
+//     }else{
+//         console.log(b-a)
+
+//     }
+    
