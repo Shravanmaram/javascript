dbdafaf1c8fb30ea444403ae6d0706a7969a2ff6
Maram Shravan Kumar <shravanmaram4@gmail.com>
2024-07-02 08:09:31 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,25 @@
+let a="shravan"
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+// console.log(a[5])
+// console.log(a[6])
+let names="maram"
+//first method
+console.log("he is "+a+"kumar"+ names)
+// second
+console.log(`he is ${a}kumar${names}`)
+//functions
+let c="prabhas"
+console.log(c.toUpperCase())
+console.log(c.toLowerCase())
+console.log(c.slice(2,7))
+console.log(c.length)
+console.log(c.slice(1))
+console.log(c.replace("ha","os"))
+console.log(a.indexOf(4))
+// etc....
+
+
